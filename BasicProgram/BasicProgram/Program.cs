7f67c1ef049bec9c0a8ec18420e8160109aeb19c
Mani <manisoft55@gmail.com>
2022-07-26 14:10:18 +0530

    Basic program

---
+++ BasicProgram/BasicProgram/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BasicProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Basic Program");
+
+                // Instance
+                SampleClass objClass = new SampleClass();
+                int totValue = objClass.ForLoop(5);
+                Console.WriteLine(totValue);
+                string outputOne = objClass.IfElseCommand(0);
+                string outputTwo = objClass.IfElseCommand(10);
+
+                SampleClass objClassOne = new SampleClass();
+
+
+                int totalValue = objClass.Addition(10, 10);
+                string output = objClass.SampleMethodPublic();
+
+                int inp = 0;
+                string j = Console.ReadLine();
+                inp = Convert.ToInt32(j);
+
+                j = Convert.ToString(inp);
+
+                string test = "";
+                int i = 5;
+                char c = 'a';
+                double d = 12.34444343434343;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ BasicProgram/BasicProgram/SampleClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    internal class SampleClass
+    {
+        // Private
+        // Public
+        // Protected
+        // Internal
+        public string TestVariable;
+
+        private string SampleMethod()
+        {
+            return "PrivateMethod";
+        }
+
+        /// <summary>
+        /// This method returns sample text
+        /// </summary>
+        /// <returns>string format</returns>
+        public string SampleMethodPublic()
+        {
+            // Below line is adding two integer number
+            int i = 10 + 10;
+            return "SampleMethodPublic";
+        }
+
+        /// <summary>
+        /// It returns addition of two number
+        /// </summary>
+        /// <param name="numberOne"></param>
+        /// <param name="numberTwo"></param>
+        /// <returns>Total Value</returns>
+        public int Addition(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+
+        public string IfElseCommand(int num)
+        {
+            if (num == 0)
+            {
+                return "This number is zero";
+            }
+            else
+            {
+                return "This number is greater that Zero.";
+            }              
+        }
+
+        public int ForLoop(int num)
+        {
+            int total = 0;
+            for(int init = 0; init < num; init++)
+            {
+                total = total + init;
+            }
+            return total;
+        }
+    }
+}
