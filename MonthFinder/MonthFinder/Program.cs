b4f1b3638e4560fc5ae220ee0ce36bee0894df31
Mani <manisoft55@gmail.com>
2022-07-30 12:05:27 +0530

    Month finder

---
+++ MonthFinder/MonthFinder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter month number");
+            string inputstring=Console.ReadLine();
+            FindMonth obj=new FindMonth();
+            string monthname =obj.ReturnMonthName(Convert.ToInt32(inputstring));
+            Console.WriteLine(monthname);
+            Console.ReadLine();
+        }
+    }
+}
