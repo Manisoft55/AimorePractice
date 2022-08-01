6352f39187c2b5104a2b749449580be98337a488
Mani <manisoft55@gmail.com>
2022-08-01 14:09:52 +0530

    array samples

---
+++ ArrayExample/ArrayExample/ArrExercise.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    internal class ArrExercise
+    {
+        public string[] ReturnSampleArray()
+        {
+            string[] arrString = new string[3];
+            arrString[0] = "Test Zero";
+            arrString[1] = "Test One";
+            arrString[2] = "Test two";
+            return arrString;
+        }
+
+        public string[] ReturnSampleArrayOne()
+        {
+            string[] arrString = { "Test Zero", "Test One" };
+            return arrString;
+        }
+
+        public int[] ReturnSampleArrayInteger()
+        {
+            int[] arrInt = { 1, 20, 13, 674, 5, 120, 456 };
+            arrInt[4] = 50;
+            return arrInt;
+        }
+    }
+}
---
+++ ArrayExample/ArrayExample/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Printing Array");
+
+            ArrExercise arrExerciseOne = new ArrExercise();
+            string[] arrValueOne = arrExerciseOne.ReturnSampleArray();
+            for (int i = 0; i < arrValueOne.Length; i++)
+            {
+                Console.WriteLine(arrValueOne[i]);
+            }
+
+            Console.WriteLine("Printing value using foreach");
+            foreach (var arr in arrValueOne)
+            {
+                Console.WriteLine(arr);
+            }
+
+
+            Console.ReadLine();
+
+            Console.WriteLine("Find Max Value");
+            int numberOne = 10;
+            int numberTwo = 20;
+            int maxNumber = Math.Max(numberOne, numberTwo);
+            Console.WriteLine("Max Number : " + maxNumber);
+
+            int minNumber = Math.Min(numberOne, numberTwo);
+            Console.WriteLine("Min Number : " + minNumber);
+            Console.ReadLine();
+
+            double absNumber = -10.34534343;
+            Console.WriteLine(Math.Abs(absNumber));
+
+            Console.ReadLine();
+            Console.WriteLine("Printing Integer Array");
+            ArrExercise obj = new ArrExercise();
+            int[] arrObj = obj.ReturnSampleArrayInteger();
+            foreach(int num in arrObj)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("Min Value");
+            Console.WriteLine(arrObj.Min());
+
+            Console.WriteLine("Max Value");
+            Console.WriteLine(arrObj.Max());
+
+            Console.WriteLine("Sum Value");
+            Console.WriteLine(arrObj.Sum());
+
+
+            Console.WriteLine("Printing Array One");
+
+            ArrExercise arrExercise = new ArrExercise();
+            string[] arrValue = arrExercise.ReturnSampleArrayOne();
+            for (int i = 0; i < arrValue.Length; i++)
+            {
+                Console.WriteLine(arrValue[i]);
+            }
+
+            Console.WriteLine("Printing value using foreach");
+            foreach (var arr in arrValue)
+            {
+                Console.WriteLine(arr);
+            }
+
+            Console.ReadLine();
+
+           
+        }
+    }
+}
