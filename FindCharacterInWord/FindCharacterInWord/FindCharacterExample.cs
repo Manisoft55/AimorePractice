96d91072c3bc6efc8e3a746fa1c4cb49987279fa
Mani <manisoft55@gmail.com>
2022-08-04 13:23:51 +0530

    find character

---
+++ FindCharacterInWord/FindCharacterInWord/FindCharacterExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindCharacterInWord
+{
+    internal class FindCharacterExample
+    {
+        internal int FindCharacterCount(string inputString, char inputChar)
+        {
+            int repCount = 0;
+            char[] charArr = inputString.ToCharArray();
+            foreach(var charElement in charArr)
+            {
+                if(charElement == inputChar)
+                    repCount++;
+            }
+            return repCount;
+        }
+    }
+}
---
+++ FindCharacterInWord/FindCharacterInWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindCharacterInWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your string");
+                string inputString = Console.ReadLine();
+                Console.WriteLine("Please enter your character");
+                char charVariable = Convert.ToChar(Console.ReadLine());
+                FindCharacterExample example = new FindCharacterExample();
+                int result = example.FindCharacterCount(inputString, charVariable);
+                Console.WriteLine($"Given character ' {charVariable} ' repeated in the word \" {inputString} \" for {result} times");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }           
+        }
+    }
+}
