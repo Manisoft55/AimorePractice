56f7b4eaaee211feed0373bb78acbd36e02d6ed2
Mani <manisoft55@gmail.com>
2022-09-11 12:16:36 +0530

    ado.net

--- UserManagement/UserManagement/Controllers/UserInfoController.cs
+++ UserManagement/UserManagement/Controllers/UserInfoController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using UserManagement.Filters;
 
@@ -11,8 +14,30 @@ namespace UserManagement.Controllers
     {
         public IActionResult Index()
         {
-           
+            string sql = "select * from employees where employee_id =@empid";
+            string sqlUpdate = "update employees set email = @emailId where employee_id = @empid";
+            SqlConnection connection = new SqlConnection("Data Source=MSP-LAPTOP;Initial Catalog=InstituteCmd;persist security info=True; Integrated Security = SSPI;");
+            SqlConnection connectionOne = new SqlConnection("Data Source=MSP-LAPTOP;Initial Catalog=InstituteCmd;persist security info=True; Integrated Security = SSPI;");
 
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlUpdate, connection);
+                command.Parameters.Add(new SqlParameter("empid", 101));
+                command.Parameters.Add(new SqlParameter("emailId", "emailidfrommvc@gmail.com"));
+                command.ExecuteNonQuery();
+                //adapter.SelectCommand = command;
+                //adapter.Fill(ds);
+                adapter.Dispose();
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                
+            }
             return View();
         }
 
