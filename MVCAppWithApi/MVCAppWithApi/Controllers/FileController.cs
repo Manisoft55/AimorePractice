0255d7246a4ae4d4b53aed348cc950e8ddd7f117
Mani <manisoft55@gmail.com>
2022-09-27 09:33:00 +0530

    doc updated

---
+++ MVCAppWithApi/MVCAppWithApi/Controllers/FileController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCAppWithApi.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAppWithApi.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FileController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]        
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            string path = @"D:\Aimore\Projects\MVCAppWithApi\MVCAppWithApi\";
+            long size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    
+                    // full path to file in temp location
+                    var filePath = path + formFile.FileName; //we are using Temp file name just for the example. Add your own file path.
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+            return Ok(new { count = files.Count, size, filePaths });
+        }
+    }
+}
---
+++ MVCAppWithApi/MVCAppWithApi/Models/FileModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCAppWithApi.Models
+{
+    public class FileModel
+    {
+        [Required(ErrorMessage = "Please select file.")]
+        [RegularExpression(@"([a-zA-Z0-9\s_\\.\-:])+(.png|.jpg|.gif)$", ErrorMessage = "Only Image files allowed.")]
+        public IFormFile PostedFile { get; set; }
+    }
+}
